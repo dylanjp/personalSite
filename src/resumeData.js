dd9892ebc1d6e8a949385df7c0d58c9e51f5911f
Dylan John Pratt <dylanjohn0506@gmail.com>
2022-08-20 20:57:05 -0600

    first commit

---
+++ src/resumeData.js
@@ -0,0 +1,103 @@
+let resumeData = {
+    "imagebaseurl":"https://github.com/dylanjp",
+    "name": "Dylan John Pratt",
+    "role": "Full Stack Developer",
+    "linkedinId":"Your LinkedIn Id",
+    "skypeid": "Your skypeid",
+    "roleDescription": " I handle website development and hosting for my clients. I love adding value and using techology to solve problems.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/dylanjohnpratt/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/dylanjp",
+          "className":"fa fa-github"
+        },
+        // {
+        //   "name":"skype",
+        //   "url":"http://twitter.com/rbhatia46",
+        //   "className":"fa fa-twitter"
+        // }
+      ],
+    "aboutme":"I'm a Software Engineer who graduated from Brigham Young University - Idaho. I've been a professional Web Developer since 2018. I love learning about new things and techologies. I'm also experienced with Game Development and Unreal Engine.",
+    "address":"Utah",
+    "website":"dylanjohn0506@gmail.com",
+    "education":[
+      {
+        "UniversityName":"Brigham Young University-Idaho",
+        "specialization":"Software Engineering",
+        // "MonthOfPassing":"Aug",
+        "YearOfPassing":"2018",
+        "Achievements":"Minor in Business Administration"
+      }
+    ],
+    // "work":[
+    //   {
+    //     "CompanyName":"The Church of Jesus Christ of Latter-day Saints",
+    //     // "specialization":"Software Engineer",
+    //     // "MonthOfLeaving":"Jan",
+    //     // "YearOfLeaving":"2018",
+    //     "Achievements":"Software Engineer"
+    //   },
+    //   {
+    //     "CompanyName":"Some Company",
+    //     "specialization":"Some specialization",
+    //     "MonthOfLeaving":"Jan",
+    //     "YearOfLeaving":"2018",
+    //     "Achievements":"Some Achievements"
+    //   }
+    // ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    // "portfolio":[
+    //   {
+    //     "name":"project1",
+    //     "description":"mobileapp",
+    //     "imgurl":"images/portfolio/phone.jpg"
+    //   },
+    //   {
+    //     "name":"project2",
+    //     "description":"mobileapp",
+    //     "imgurl":"images/portfolio/project.jpg"
+    //   },
+    //   {
+    //     "name":"project3",
+    //     "description":"mobileapp",  
+    //     "imgurl":"images/portfolio/project2.png"
+    //   },
+    //   {
+    //     "name":"project4",
+    //     "description":"mobileapp",
+    //     "imgurl":"images/portfolio/phone.jpg"
+    //   }
+    // ],
+    "testimonials":[
+      {
+        "description":"Dylan is fantastic to work with. He is super responsive, is a great listener, has creative ideas, is tenacious at solving techy mysteries, and has complete follow through. And he is fun to be with! I hope to work with him again in the future!",
+        "name":"Adrienne Chamberlain"
+      },
+      // {
+      //   "description":"This is a sample testimonial",
+      //   "name":"Some technical guy"
+      // },
+      {
+        "description":"Dylan was incredible to work with. He created a website for me that was not only personal and knowledgable of my industry, but also visually appealing and well done. As he is a highly skilled software engineer, it was delivered quickly, and without flaws. I should further note that his vocational integrity is clear when working with clients and his bright demeanor and ease of communication makes every interaction enjoyable. I highly recommend Dylan's services to anyone who needs a quality product delivered efficiently and professionally.",
+        "name":"Dalton Smith"
+      }
+    ]
+  }
+  
+  export default resumeData
